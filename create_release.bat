9cf8e61002dd135bec581d529ad68b625efaba08
hogsy <hogsy@oldtimes-software.com>
2023-01-18 17:48:00 +0000

    Include the changes list.

--- create_release.bat
+++ create_release.bat
@@ -1,4 +1,4 @@
 @echo off
 del hogsy_jade-patch.zip
-zip hogsy_jade-patch.zip mappings/* Jade_edd.exe SDL2.dll
+zip hogsy_jade-patch.zip mappings/* changes.txt Jade_edd.exe SDL2.dll
 echo %errorlevel%
