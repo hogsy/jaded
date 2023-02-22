9a246bd6e666d8acc35d82eb21f25c7cf8b69e1d
hogsy <hogsy@oldtimes-software.com>
2023-02-22 08:45:50 +0000

    Updated release batch to also pack source code.

--- create_release.bat
+++ create_release.bat
@@ -1,4 +1,7 @@
 @echo off
-del hogsy_jade-patch.zip
-zip hogsy_jade-patch.zip mappings/* changes.txt JadedEditor.exe SDL2.dll
+set datevar=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
+del releases/hogsy_jade-patch_%datevar%.zip
+zip releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe SDL2.dll
+del releases/hogsy_jade-source_%datevar%.zip
+zip releases/hogsy_jade-source_%datevar%.zip -r Main/ Dlls/ Extern/ Libraries/ Tools/ -i *.rc *.bmp *.ico *.c *.cpp *.h *.sln *.vcxproj *.vcxproj.filters
 echo %errorlevel%
