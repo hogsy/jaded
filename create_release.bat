b1bb13fc99299a85f7e5e57b83137963592258bc
hogsy <hogsy@oldtimes-software.com>
2023-05-02 11:59:41 +0100

    Move web resources over to this repo.

--- create_release.bat
+++ create_release.bat
@@ -1,7 +1,8 @@
 @echo off
 set datevar=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
-del releases/hogsy_jade-patch_%datevar%.zip
-zip releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe SDL2.dll
-del releases/hogsy_jade-source_%datevar%.zip
-zip releases/hogsy_jade-source_%datevar%.zip -r Main/ Dlls/ Extern/ Libraries/ Tools/ -i *.rc *.bmp *.ico *.c *.cpp *.h *.sln *.vcxproj *.vcxproj.filters
+del web/releases/hogsy_jade-patch_%datevar%.zip
+zip web/releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe SDL2.dll
+del web/releases/hogsy_jade-source_%datevar%.zip
+zip web/releases/hogsy_jade-source_%datevar%.zip -r Main/ Dlls/ Extern/ Libraries/ Tools/ -i *.rc *.bmp *.ico *.c *.cpp *.h *.sln *.vcxproj *.vcxproj.filters
+copy changes.txt web/releases/changes.txt /Y
 echo %errorlevel%
