5745646ced0fe4997e05926f63015c3406242ab9
hogsy <hogsy@oldtimes-software.com>
2023-05-13 11:25:45 +0100

    Blah to storing enginedatas on the repo. Removed some more crap we don't need. CMake files. Made a tiny tiny start on Water crap for GL.

---
+++ Libraries/OpenGL/Sources/OGLWater.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mark- on 05/05/2023.
+//
+
+#include "OGLWater.h"
---
+++ Libraries/OpenGL/Sources/OGLWater.h
@@ -0,0 +1,8 @@
+//
+// Created by mark- on 05/05/2023.
+//
+
+#ifndef JADED_OGLWATER_H
+#define JADED_OGLWATER_H
+
+#endif //JADED_OGLWATER_H
