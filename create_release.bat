ec0b395b5d867332d8db7f689ed4b61bda1256e5
hogsy <hogsy@oldtimes-software.com>
2023-09-09 23:58:44 +0100

    Include new batches for release dump.

--- create_release.bat
+++ create_release.bat
@@ -1,8 +1,8 @@
 @echo off
 set datevar=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
 del web/releases/hogsy_jade-patch_%datevar%.zip
-zip web/releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe SDL2.dll legal.txt
+zip web/releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe run_console.bat run_editor.bat SDL2.dll legal.txt
 del web/releases/hogsy_jade-source_%datevar%.zip
-zip web/releases/hogsy_jade-source_%datevar%.zip -r Main/ Dlls/ Extern/ Libraries/ Tools/ -i *.rc *.bmp *.ico *.c *.cpp *.h *.sln *.vcxproj *.vcxproj.filters
+zip web/releases/hogsy_jade-source_%datevar%.zip -r Main/ Dlls/ Extern/ Libraries/ Tools/ -i *.rc *.bat *.bmp *.ico *.c *.cpp *.h *.sln *.vcxproj *.vcxproj.filters
 copy changes.txt web/releases/changes.txt /Y
 echo %errorlevel%
