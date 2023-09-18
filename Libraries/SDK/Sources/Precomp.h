102756abae86ecb16ac3d62547503b9458a279c3
Mark Sowden <hogsy@oldtimes-software.com>
2023-09-18 19:24:01 +0100

    Should probably exclude CLIwin.h on non-windows targets.

--- Libraries/SDK/Sources/Precomp.h
+++ Libraries/SDK/Sources/Precomp.h
@@ -1,19 +1,13 @@
 #ifndef UBI_INCLUDED_PRECOMP_H__D782E0F5_652A_419C_85B9_DF5E846A2780
 #define UBI_INCLUDED_PRECOMP_H__D782E0F5_652A_419C_85B9_DF5E846A2780
 
-#ifdef JADEFUSION
-#pragma message("*******************************************************************************")
-#pragma message("*** SDK Precompiled Header ****************************************************")
-
-#include "projectconfig.h"
+#if defined( _WIN32 )
+#	include "BASe/CLIbrary/CLIwin.h"
 #endif
-
-#include "BASe/CLIbrary/CLIwin.h"
 #include "BASe/CLIbrary/CLIfile.h"
 #include "BASe/CLIbrary/CLIstr.h"
 #include "BASe/CLIbrary/CLImem.h"
 #include "BASe/CLIbrary/CLIxxx.h"
 #include "ENGine/Sources/OBJects/OBJstruct.h"
 
-#endif	//UBI_INCLUDED_PRECOMP_H__D782E0F5_652A_419C_85B9_DF5E846A2780
-
+#endif//UBI_INCLUDED_PRECOMP_H__D782E0F5_652A_419C_85B9_DF5E846A2780
