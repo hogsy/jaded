d5a27b5274ca76f885803e05726279f7b48c2a9d
Mark Sowden <markelswo@gmail.com>
2025-07-05 17:10:37 +0100

    Update changelog and release batch.

--- create_release.bat
+++ create_release.bat
@@ -1,8 +1,6 @@
 @echo off
 set datevar=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
 del web/releases/hogsy_jade-patch_%datevar%.zip
-zip web/releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe run_window.bat run_console.bat run_editor.bat SDL2.dll legal.txt
-del web/releases/hogsy_jade-source_%datevar%.zip
-zip web/releases/hogsy_jade-source_%datevar%.zip -r Main/ Dlls/ Extern/ Libraries/ Tools/ -i *.rc *.bat *.bmp *.ico *.c *.cpp *.h *.sln *.vcxproj *.vcxproj.filters
+zip -9 web/releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe Jaded.pdb run_window.bat run_console.bat run_editor.bat SDL3.dll legal.txt
 copy changes.txt web/releases/changes.txt /Y
 echo %errorlevel%
