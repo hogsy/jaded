6050acd1e3f660a60a27a2193d42579265e3d3b3
Mark Sowden <markelswo@gmail.com>
2025-11-07 19:14:09 +0000

    Shuffled crap around.

--- Main/Shared/Renderer/ShaderManager.cpp
+++ Main/Shared/Renderer/ShaderManager.cpp
@@ -1,117 +1,13 @@
 // Created by Mark "hogsy" Sowden, 2025 <hogsy@oldtimes-software.com>
 // https://oldtimes-software.com/jaded/
 
-#include <vector>
-#include <string>
-#include <map>
-
 #include <GL/glew.h>
 
-#include "../Main/Shared/MainSharedApp.h"
+#include "../MainSharedApp.h"
 
 #include "LINks/LINKmsg.h"
 
-namespace jaded
-{
-	namespace renderer
-	{
-		// originally the plan here was to make this an interface, and then
-		// build API-specific classes on top, but we'll consider this later...
-
-		class GLShaderProgramStage
-		{
-			int id_{};
-
-			GLenum type_{ 0 };
-
-			std::string path_;
-
-			GLShaderProgramStage( GLenum type, const std::string &path );
-			~GLShaderProgramStage();
-		};
-
-		union GLShaderProgramValue
-		{
-			int32_t i32;
-			int8_t  i8;
-
-			float  f32;
-			double f64;
-
-			float f32v2[ 2 ];
-			float f32v3[ 3 ];
-			float f32v4[ 4 ];
-
-			float f32m3[ 3 ][ 3 ];
-			float f32m4[ 4 ][ 4 ];
-		};
-
-		class GLShaderProgram
-		{
-			int id_{};
-
-			struct Uniform
-			{
-				int id{};
-
-				GLenum type{};
-
-				std::string name;
-
-				GLShaderProgramValue defaultValue;
-				GLShaderProgramValue value;
-			};
-
-			struct Attribute
-			{
-				int id{};
-
-				GLenum type{};
-
-				std::string name;
-			};
-
-			std::map< std::string, Uniform >   uniforms_;
-			std::map< std::string, Attribute > attributes_;
-
-		public:
-			GLShaderProgram();
-			~GLShaderProgram();
-
-			void Reload();
-			void MakeActive();
-
-			int GetUniform( const std::string &name );
-			int GetAttribute( const std::string &name );
-
-		private:
-			bool Link();
-
-			void PopulateUniforms();
-			void PopulateAttributes();
-
-		public:
-		};
-
-		class GLShaderManager
-		{
-			static constexpr unsigned int HOT_RELOAD_TIMER = 60;
-
-			bool         hotReload_{ false };
-			unsigned int incHotReloadTicks_{ HOT_RELOAD_TIMER };
-			unsigned int hotReloadTicks_{ HOT_RELOAD_TIMER };
-
-			std::map< std::string, GLShaderProgram * > programs_;
-
-		public:
-			void HotReloadPrograms();
-
-			void SetupDefaults();
-
-			GLShaderProgram *GetProgram( const std::string &name );
-		};
-	}// namespace renderer
-}// namespace jaded
+#include "ShaderManager.h"
 
 ///////////////////////////////////////////////////////////////////
 // Shader Program
@@ -122,9 +18,12 @@ jaded::renderer::GLShaderProgramStage::GLShaderProgramStage( GLenum type, const 
 	id_ = glCreateShader( type );
 	if ( id_ == 0 )
 	{
-		LINK_PrintStatusMsg( "Failed to create shader stage!" );
-		return;
+		throw std::runtime_error( "failed to create shader stage" );
 	}
+}
+
+jaded::renderer::GLShaderProgramStage::~GLShaderProgramStage()
+{
 }
 
 jaded::renderer::GLShaderProgram::GLShaderProgram()
@@ -132,8 +31,7 @@ jaded::renderer::GLShaderProgram::GLShaderProgram()
 	id_ = glCreateProgram();
 	if ( id_ == 0 )
 	{
-		LINK_PrintStatusMsg( "Failed to create shader program!" );
-		return;
+		throw std::runtime_error( "failed to create shader program" );
 	}
 }
 
