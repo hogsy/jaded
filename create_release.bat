ad853b6fdfdd94972ff016c4d8a1cf8ce252eccf
hogsy <markelswo@gmail.com>
2025-11-19 10:33:51 +0000

    Removed web dir and amended create release batch - web has all just been moved to my blog instead.

--- create_release.bat
+++ create_release.bat
@@ -1,6 +1,5 @@
 @echo off
 set datevar=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
-del web/releases/hogsy_jade-patch_%datevar%.zip
-zip -9 web/releases/hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe Jaded.pdb run_window.bat run_console.bat run_editor.bat SDL3.dll legal.txt
-copy changes.txt web/releases/changes.txt /Y
+del hogsy_jade-patch_%datevar%.zip
+zip -9 hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe Jaded.pdb run_window.bat run_console.bat run_editor.bat SDL3.dll legal.txt
 echo %errorlevel%
