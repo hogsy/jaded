c1348efdba44e4308b0c60eccad4a204318693c8
Mark Sowden <markelswo@gmail.com>
2026-01-10 12:35:59 +0000

    Move things over to runtime output dir

--- create_release.bat
+++ create_release.bat
@@ -1,5 +1,5 @@
 @echo off
 set datevar=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
 del hogsy_jade-patch_%datevar%.zip
-zip -9 hogsy_jade-patch_%datevar%.zip mappings/* changes.txt Jaded.exe Jaded.pdb run_window.bat run_console.bat run_editor.bat SDL3.dll legal.txt
+zip -9 hogsy_jade-patch_%datevar%.zip runtime/mappings/* runtime/shaders/glsl/* changes.txt runtime/Jaded.exe runtime/Jaded.pdb runtime/run_window.bat runtime/run_console.bat runtime/run_editor.bat runtime/SDL3.dll runtime/legal.txt
 echo %errorlevel%
