8a3c097e669324e07ad99298911755f5cf27b0c5
Mark Sowden <markelswo@gmail.com>
2026-01-31 18:33:22 +0000

    Added unpacker tool.
    Signed-off-by: Mark Sowden <markelswo@gmail.com>

---
+++ src/tools/unpacker/unpacker.cpp
@@ -0,0 +1,22 @@
+#include "jaded/core/fs/fs_pak.h"
+
+int main( int argc, char **argv )
+{
+	if ( argc <= 1 )
+	{
+		printf( "Invalid number of arguments!\n" );
+		return EXIT_FAILURE;
+	}
+
+	const std::string path = argv[ 1 ];
+
+	core::fs::Pak pak;
+	if ( !pak.Open( path ) )
+	{
+		return EXIT_FAILURE;
+	}
+
+	pak.Export( "Dump" );
+
+	return EXIT_SUCCESS;
+}
